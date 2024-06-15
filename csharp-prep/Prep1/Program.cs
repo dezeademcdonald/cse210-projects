9613c5eb92b5bf248d08ff0ebafbfb8923e032af
McDonald W De-zeade <mcdonald00dezeade@gmail.com>
2024-06-15 02:38:19 +0100

    Finished Pre 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine();
+
+        Console.Write("What is your first name? ");
+        string FirstName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string LastName = Console.ReadLine();
+
+        Console.Write($"Your full name is {LastName}, {FirstName} {LastName}.");
     }
 }
