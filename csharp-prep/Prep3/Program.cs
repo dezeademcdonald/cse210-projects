21e1b633475abd2791d58095105b8d34d8e0306f
McDonald W De-zeade <mcdonald00dezeade@gmail.com>
2024-06-15 14:26:45 +0100

    added a comment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        for (int i = 1; i <= 12; i = i * 2)
+        {
+            Console.WriteLine(i);
+        }
     }
 }
